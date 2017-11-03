8ca8fa4ca3282658244e65b17c7ef08ddadb64f0
Jason <jasonchiewsam@gmail.com>
2017-11-03 17:09:27 +0100

    Title Scene
    Added Scene for Title Screen, need to improve that.

---
+++ Assets/Scripts/MenuSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSettings : MonoBehaviour
+{
+    public bool fullscreen = false;
+
+    public void Resolution(int res)
+    {
+        if (res == 1) Screen.SetResolution(1920, 1080, fullscreen);
+        if (res == 2) Screen.SetResolution(1280, 720, fullscreen);
+        if (res == 3) Screen.SetResolution(800, 600, fullscreen);
+    }
+    public void Quality(int qua)
+    {
+        QualitySettings.SetQualityLevel(qua);
+    }
+}

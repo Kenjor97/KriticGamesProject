e1d31d97628ff47f4260b9a9d40f0077776db49f
Jason <jasonchiewsam@gmail.com>
2017-12-14 16:32:46 +0100

    Ranged Attack Update
    Ranged Attack now appears in front of the player.
    Projectile still doesn't destroys when collide with ground...

--- Assets/Scripts/ProjectileBehaviour.cs
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -7,12 +7,15 @@ public class ProjectileBehaviour : MonoBehaviour
     public Transform projectile;
     public CharacterBehaviour player;
     public float speed = 7.5f;
-    public float rotateSpeed = -500;
+    public float rotateSpeed = 500;
+    public bool facingRight;
 
     void Start ()
     {
         projectile = GetComponent<Transform>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterBehaviour>();
+        if (player.isFacingRight) facingRight = true;
+        else facingRight = false;
     }
 	
 	void Update ()
@@ -22,8 +25,16 @@ public class ProjectileBehaviour : MonoBehaviour
 
     private void FixedUpdate()
     {
-        projectile.localPosition = new Vector3(projectile.localPosition.x + Time.deltaTime * speed, projectile.localPosition.y, projectile.localPosition.z);
-        projectile.Rotate(Vector3.forward * Time.deltaTime * rotateSpeed);
+        if (facingRight)
+        {
+            projectile.localPosition = new Vector3(projectile.localPosition.x + Time.deltaTime * speed, projectile.localPosition.y, projectile.localPosition.z);
+            projectile.Rotate(Vector3.forward * Time.deltaTime * -rotateSpeed);
+        }
+        else
+        {
+            projectile.localPosition = new Vector3(projectile.localPosition.x - Time.deltaTime * speed, projectile.localPosition.y, projectile.localPosition.z);
+            projectile.Rotate(Vector3.forward * Time.deltaTime * rotateSpeed);
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {

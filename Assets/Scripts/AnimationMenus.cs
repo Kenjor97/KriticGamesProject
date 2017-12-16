ac3cb9e8c856579891be0ae3f4e9f78aa5634d64
Diego <32306681+DiegoDCV@users.noreply.github.com>
2017-12-16 18:36:37 +0100

    Added basic "Press Enter to Start" animation
    Only enable or disable the text

--- Assets/Scripts/AnimationMenus.cs
+++ Assets/Scripts/AnimationMenus.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class AnimationMenus : MonoBehaviour
 {
-    Animator Table;
-    Animation WoodTable;
+    public MouseCursor cursor;
+    public Animator woodTable;
+    public Animator anvil;
 
 	// Use this for initialization
 	void Start ()
     {
-        
-		
+        cursor = GetComponent<MouseCursor>();
+        woodTable.enabled = false;
+        anvil.enabled = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if(Input.GetKeyDown(KeyCode.P))
+
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-        WoodTable.Stop();
-            Debug.Log("Pulsado P");
+            Debug.Log("Pressed Enter");
+            woodTable.enabled = true;
+            anvil.enabled = true;
+            anvil.Rebind();
+            woodTable.Rebind();
         }
-        
-        
-	}
+
+    }
 
 
 }

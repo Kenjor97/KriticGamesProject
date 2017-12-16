bf8b80120d1498d8f9d4ebd5c60a73a5de51a4da
Diego <32306681+DiegoDCV@users.noreply.github.com>
2017-12-16 22:52:53 +0100

    Logo Screen

--- Assets/Scripts/FadeOutScript.cs
+++ Assets/Scripts/FadeOutScript.cs
@@ -6,10 +6,16 @@ public class FadeOutScript : MonoBehaviour
 
     public Texture2D fadeOutTexture;    // the texture that will overlay the screen. This can be a black image or a loading graphic
     public float fadeSpeedOut = 0.8f;      // the fading speed
-
+    public Fade fade;
     private int drawDepthOut = -1000;      // the texture's order in the draw hierarchy: a low number means it renders on top
     private float alphaOut = 1.0f;         // the texture's alpha value between 0 and 1
     private int fadeDirOut = -1;           // the direction to fade: in = -1 or out = 1
+    public GameObject fadeOutObject;
+
+    private void Start()
+    {
+        fadeOutObject.SetActive(false);
+    }
 
     void OnGUI()
     {
@@ -21,7 +27,7 @@ public class FadeOutScript : MonoBehaviour
         // set color of our GUI (in this case our texture). All color values remain the same & the Alpha is set to the alpha variable
         GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alphaOut);
         GUI.depth = drawDepthOut;                                                              // make the black texture render on top (drawn last)
-        GUI.DrawTexture(new Rect(Screen.width / 3, 0, fadeOutTexture.width / 3, fadeOutTexture.height / 3), fadeOutTexture);       // draw the texture to fit the entire screen area
+        GUI.DrawTexture(new Rect(Screen.width/3, 0, fadeOutTexture.width/3, fadeOutTexture.height/3), fadeOutTexture);       // draw the texture to fit the entire screen area
     }
 
     // sets fadeDir to the direction parameter making the scene fade in if -1 and out if 1
@@ -31,13 +37,14 @@ public class FadeOutScript : MonoBehaviour
         return (fadeSpeedOut);
     }
 
-    // OnLevelWasLoaded is called when a level is loaded. It takes loaded level index (int) as a parameter so you can limit the fade in to certain scenes.
-    void OnLevelWasLoaded()
+    private void Update()
     {
-        // alpha = 1;		// use this if the alpha is not set to 1 by default
-        BeginFade(-1);      // call the fade in function
-
+            BeginFade(-1);
     }
 
 
+    public void Activate()
+    {
+        fadeOutObject.SetActive(true);
+    }
 }

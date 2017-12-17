8a759bd7341292fc81343a9a4897b59d1c2b468e
Jason <jasonchiewsam@gmail.com>
2017-12-17 04:44:39 +0100

    Basic UI Update
    Implemented Basic UI with Player's life as hearts.

---
+++ Assets/Scripts/HeartHUD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartHUD : MonoBehaviour
+{
+    public Sprite[] heartSprites;
+    public Image heartUI;
+    public CharacterBehaviour player;
+
+	void Start ()
+    {
+        heartUI = GameObject.FindGameObjectWithTag("Hearts").GetComponent<Image>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterBehaviour>();
+    }
+	
+	void Update ()
+    {
+        heartUI.sprite = heartSprites[player.life];
+	}
+}

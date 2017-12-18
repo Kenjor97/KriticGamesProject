5e08ddb8b37e78b15ab53ab89a06169e27b3b0e2
Jason <jasonchiewsam@gmail.com>
2017-12-18 05:02:16 +0100

    Ending Screen
    Implemented Ending Victory/Defeat Screen

---
+++ Assets/Scripts/Screens.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Screens : MonoBehaviour
+{
+    public CharacterBehaviour player;
+    public Boss1Behaviour boss;
+    
+	void Start ()
+    {
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterBehaviour>();
+		boss = GameObject.FindGameObjectWithTag("Boss").GetComponent<Boss1Behaviour>();
+    }
+	
+	void Update ()
+    {
+        if(player.state == CharacterBehaviour.State.Dead) SceneManager.LoadScene("3_EndingLose");
+        else if(boss.state == Boss1Behaviour.State.Dead) SceneManager.LoadScene("2_EndingWin");
+    }
+    
+}

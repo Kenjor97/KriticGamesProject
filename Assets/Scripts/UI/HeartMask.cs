73113c4d47438ef85ba1ce4cfebf6681fa559967
Jason <jasonchiewsam@gmail.com>
2018-01-31 19:30:34 +0100

    Life HUD Update
    Player Life now shows the actual max life.

---
+++ Assets/Scripts/UI/HeartMask.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartMask : MonoBehaviour
+{
+    public CharacterBehaviour player;
+    public RectTransform rt;
+    public float fourHearts;
+    public float fiveHearts;
+
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterBehaviour>();
+        rt = GetComponent<RectTransform>();
+	}
+	
+	void Update ()
+    {
+		if(player.maxLife == 4)
+        {
+            rt.sizeDelta = new Vector2(fourHearts, rt.sizeDelta.y);
+        }
+        if(player.maxLife == 5)
+        {
+            rt.sizeDelta = new Vector2(fiveHearts, rt.sizeDelta.y);
+        }
+    }
+}

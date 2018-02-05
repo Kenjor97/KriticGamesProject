27b0bdf16eedf49c7ebb59e370457811389bd378
Jason <jasonchiewsam@gmail.com>
2018-02-05 19:11:46 +0100

    Implemented Boots Power Up Placeholder
    Implemented a placeholder for the boots power up.
    Implemented boots functionality.
    Deleted some unused sprites.
    Changed again player prefab in testing zone scene.

---
+++ Assets/Scripts/Player/BootsPowerUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BootsPowerUp : MonoBehaviour
+{
+    public CharacterBehaviour player;
+
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterBehaviour>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("player"))
+        {
+            player.hasBoots = true;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
